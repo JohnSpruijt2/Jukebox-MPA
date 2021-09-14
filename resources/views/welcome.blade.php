2126d127aa9569d0a9a484c6ebcc293eb587ed9f
john spruijt <johnspruijt@outlook.com>
2021-09-14 12:57:46 +0200

    added front page styling and buttons

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,16 +5,31 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Songs</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"> 
     <link href="{{ asset('css/default.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/frontpage.css') }}" rel="stylesheet">
 
 </head>
 <body>
     <main>
-    
+        <h1 class='text-black' id="headTitle">Welcome to yotify!</h1>
+
+        <div id='loginBtnDiv'>
+            <a class="btn btn-dark" href="/login">Login</a>
+            <a class="btn btn-dark" href="/register">Register</a>
+        </div>
+
+        <section id="aboutUsSection">
+            <h2>About us</h2>
+            <p>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tempus erat a nisl aliquet, et sollicitudin tortor ultrices. Mauris non ante nec mauris rutrum aliquam vitae sit amet ipsum. Cras semper erat sit amet augue ullamcorper eleifend. Integer ornare pharetra diam. Cras commodo commodo lectus, non vehicula enim ullamcorper ac. Etiam et dui sit amet arcu molestie pellentesque quis eu sem. Integer lacinia, magna et efficitur iaculis, urna urna scelerisque enim, a rhoncus mi nibh bibendum metus. Ut quis dapibus enim. Maecenas finibus pharetra ligula vel consectetur. Proin maximus pellentesque facilisis. Maecenas fringilla tellus tellus. Donec ornare dictum tellus at interdum. Duis maximus pulvinar lorem eget elementum. Duis tincidunt erat quis nisi ultrices, ac bibendum justo consectetur. Quisque a purus in ipsum consectetur convallis.
+<br> <br>
+Aenean viverra luctus diam et commodo. Phasellus elementum augue quis sem placerat, a volutpat dolor molestie. Phasellus luctus sagittis neque eget ornare. Vivamus egestas semper nunc ut scelerisque. Aenean nisi est, egestas ut tellus eget, venenatis placerat massa. Praesent a rhoncus risus. Quisque vel purus a mauris imperdiet sagittis. Mauris pulvinar lectus quis lacus consectetur, ac euismod augue facilisis. Donec vestibulum turpis sit amet orci viverra, commodo euismod lorem mattis. Integer congue, est eget tristique pharetra, purus est iaculis ipsum, id blandit enim massa a magna. Sed rutrum at nisl sed consectetur. </p>
+        </section>
     </main>
 </body>
 </html>
