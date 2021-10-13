9de2f25ba7f01d06f34b0c0baf190605a5a2da74
john spruijt <johnspruijt@outlook.com>
2021-10-13 12:15:11 +0200

    started working with eloquent

--- app/Http/Controllers/songController.php
+++ app/Http/Controllers/songController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Song;
+use App\Models\SavedList;
+use App\Models\Genre;
+
 use Illuminate\Http\Request;
 use Auth;
 use DB;
@@ -13,36 +17,16 @@ class songController extends Controller
     //
     public function index() {
         $user = Auth::user();
-        $songs = DB::select('select * from songs');
-        foreach ($songs as $song) {             //convert seconds into minutes and seconds
-            $duration = $song->duration;
-            $minutes = floor($duration/60);
-            $second = $minutes*60;
-            $seconds = $duration-$second;
-            if ($seconds <10) {
-                $seconds = '0'.$seconds;
-            }
-            $song->duration = $minutes.':'.$seconds;
-        }
-        $lists = DB::select('select * from saved_lists where userId='.$user->id);
-        $genres = DB::select('select * from `genres`');
+        $songs = Song::getSongs();
+        $lists = SavedList::where('userId', $user->id)->get();
+        $genres = Genre::all();
         return view('songs', ['songs' => $songs, 'lists' => $lists, 'playLists' => Session::get('saved_list'), 'genres' => $genres]);
     }
 
     public function genreSort(Request $request) {
         $user = Auth::user();
         $id = $request->id;
-        $songs = DB::select('select * from songs where genre_id='.$id);
-        foreach ($songs as $song) {             //convert seconds into minutes and seconds
-            $duration = $song->duration;
-            $minutes = floor($duration/60);
-            $second = $minutes*60;
-            $seconds = $duration-$second;
-            if ($seconds <10) {
-                $seconds = '0'.$seconds;
-            }
-            $song->duration = $minutes.':'.$seconds;
-        }
+        $songs = Song::getSongsByGenre($id);
         $lists = DB::select('select * from saved_lists where userId='.$user->id);
         $genres = DB::select('select * from `genres`');
         return view('songs', ['songs' => $songs, 'lists' => $lists, 'playLists' => Session::get('saved_list'), 'genres' => $genres]);
---
+++ app/Models/SavedList.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SavedList extends Model
+{
+    use HasFactory;
+    
+}
